25c81ec1a5ff900bccd77dda1fe2fcb56aaf3cd4
Daniel Marques <daniel.marques142@gmail.com>
2022-05-13 00:40:02 -0300

    feat: lesson 12 seting up node-config

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
 import { InternalError } from '@src/util/errors/internal-error';
 import { AxiosStatic } from 'axios';
+import config, { IConfig } from 'config';
 
 export interface StormGlassPointSource {
   [key: string]: number;
@@ -31,6 +32,10 @@ export interface ForecastPoint {
   windSpeed: number;
 }
 
+const stormGlassResourceConfig: IConfig = config.get(
+  'App.resources.StormGlass'
+);
+
 export class StormGlassClientResponseError extends InternalError{
   constructor(message: string){
     const internalMessage = 'Unexpected error when trying to communicate to StormGlass:';
@@ -47,11 +52,10 @@ export class StormGlass {
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
-      const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass
-      .io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this
-        .stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`, {
+      const response = await this.request.get<StormGlassForecastResponse>(`${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${this.stormGlassAPIParams}&source=
+      ${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`, {
           headers: {
-            Authorization: 'fake-token'
+            Authorization: stormGlassResourceConfig.get('apiToken')
           }
         }
       );
